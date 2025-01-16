027f99611f80ab255654aa7bc9662287411126f0
jinqichew <zceejqc@ucl.ac.uk>
2025-01-16 18:06:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from A import random_forest
-from B import cnn
+from A import random_forest_taskA
+from B import cnn_taskB
 
 def main():
-    random_forest.random_forest_A()
-    cnn.cnn_B()
+    random_forest_taskA.random_forest_A()
+    cnn_taskB.cnn_B()
 
 if __name__ == "__main__":
     main()
